2930016dff46a553493a3a198edc311d325d07f7
Kpearson72 <kpearson72@gmail.com>
2021-03-14 15:00:57 -0500

    set circles and text to new each time rendered and add function to update tool tip

--- D3_data_journalism_BONUS/assets/js/app.js
+++ D3_data_journalism_BONUS/assets/js/app.js
@@ -1,4 +1,3 @@
-    // @TODO: YOUR CODE HERE!
 // function to make responsive
 function makeResponsive(){
 
@@ -43,11 +42,87 @@ function makeResponsive(){
         // create scales
         let xLinearScale = d3.scaleLinear()
             .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
-            d3.max(censusData, d => d[chosenXAxis]) *1.2 
+            d3.max(censusData, d => d[chosenXAxis]) * 1.2 
             ])
             .range([0, width]);
         return xLinearScale;
     }
+
+    // function used for updating y-scale let upon click on axis label
+    function yScale(chosenYAxis, censusData) {
+        let yLinearScale = d3.scaleLinear()
+            .domain([d3.min(censusData, d=> d[chosenYAxis]) * 0.8,
+            d3.max(censusData, d=> d[chosenYAxis]) * 1.2
+            ])
+            .range([height,0]);
+        return yLinearScale;
+    }
+
+    // function used for updating xAxis let upon click on axis Label
+    function renderXAxes(newXScale, xAxis) {
+        let bottomAxis = d3.axisBottom(newXScale);
+
+        xAxis.transition()
+            .duration(1000)
+            .call(bottomAxis);
+
+        return xAxis;
+    }
+
+    // function used for updating yAxis let upon click on axis label
+    function renderYAxes(newYScale, yAxis) {
+        let leftAxis = d3.axisLeft(newYScale);
+
+            yAxis.transition()
+                .duration(1000)
+                .call(leftAxis);
+                
+            return yAxis;
+    }
+    // function used for updating circles group to new
+    function setCircles(circlesGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+        textGroup.transition()
+            .duration(1000)
+            .attr("x", d => newXScale(d[chosenXAxis]))
+            .attr("y", d => newYScale(d[chosenYAxis]))
+            .attr("text-anchor", "middle");
+
+        return circlesGroup;
+    }
+
+    // function used for updating text group to new
+    function setText(circlesGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+        textGroup.transition()
+            .duration(1000)
+            .attr("x", d => newXScale(d[chosenXAxis]))
+            .attr("y", d => newYScale(d[chosenYAxis]))
+            .attr("text-anchor", "middle");
+
+        return circlesGroup;
+    }
+
+    // function to update circle group using Tooltip
+    function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup, textGroup) {
+
+        if (chosenXAxis === "poverty") {
+            let xLabel = "Poverty (%)";
+        }
+        else if (chosenXAxis === "age") {
+            let xLabel = "Age (Median)";
+        }
+        else {
+            let xLabel = "Household Income (Median)";
+        }
+        if (chosenYAxis === "healthcare") {
+            let yLabel = "Lacks Healthcare (%)";
+        }
+        else if (chosenYAxis === "obesity") {
+            let yLabel = "Obese (%)";
+        }
+        else {
+            let yLabel = "Smokes (%)";
+        }
+
 
 
 }
